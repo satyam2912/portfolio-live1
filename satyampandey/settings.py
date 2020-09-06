03d16b52a8ad6291d60885cd0a32454cdfb4d0be
satyam pandey <37556595+satyam2912@users.noreply.github.com>
2020-09-07 00:30:29 +0530

    Update settings.py

--- satyampandey/settings.py
+++ satyampandey/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-     'cloudinary_storage',
+    'cloudinary_storage',
     'cloudinary',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
